c7ded5f1e94089e8010e9ba126136952ef722c9f
GDinesh <criticalopsnoob1302@gmail.com>
2021-01-31 16:31:32 +0530

    HELLO WORLD

--- exercise4-18.lisp
+++ exercise4-18.lisp
@@ -1,1 +1,14 @@
-(defun )
+;;;; 1 is paper, 2 is scissor, 3 is rock
+(defun playGame(user1 user2)
+  (cond ((and (eq user1 'SCISSORS) (eq user2 'PAPER)) "USER1 WINS")
+        ((and (eq user1 'SCISSORS) (eq user2 'ROCK)) "USER2 WINS")
+        ((and (eq user1 'SCISSORS) (eq user2 'SCISSORS)) "TIE")
+        ((and (eq user1 'PAPER) (eq user2 'SCISSORS)) "USER2 WINS")
+        ((and (eq user1 'PAPER) (eq user2 'ROCK)) "USER1 WINS")
+        ((and (eq user1 'PAPER) (eq user2 'PAPER)) "TIE")
+        ((and (eq user1 'ROCK) (eq user2 'SCISSORS)) "USER1 WINS")
+        ((and (eq user1 'ROCK) (eq user2 'PAPER)) "USER2 WINS")
+        ((and (eq user1 'ROCK) (eq user2 'ROCK)) "TIE")
+        (t "WRONG OPTION")))
+
+(format t "~d" (playGame 'SCISSORS 'PAPER))
