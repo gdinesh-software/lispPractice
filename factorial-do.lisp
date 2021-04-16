18e35d8368552529e68a5f8c844e3a4edca32555
gdinesh-software <criticalopsnoob1302@gmail.com>
2021-04-16 13:24:18 +0530

    New

--- factorial-do.lisp
+++ factorial-do.lisp
@@ -1,5 +1,15 @@
 (defun factorial (num)
   (do ((i num (1- i))
        (result 1 (* result i)))
-    ((zerop i) result)))
-(print (factorial 6))
+    ((zerop num) result)))
+
+(defun tail-recursive-fact (num)
+  (labels ((fact-helper (num acc)
+                (if (zerop num)
+                  acc
+                  (fact-helper (1- num) (* num acc)))))
+    (fact-helper num 1)))
+
+(compile 'tail-recursive-fact)
+(print (tail-recursive-fact 10000))
+
